c97cb7033484b1cd198fe4f313e82d826d443bf4
Mike Hamer <mike@mikehamer.info>
2013-01-04 18:52:18 +0100

    Updated to allow old versions of ARDroneUbuntu to update themselves

--- install_new_tutorials.sh
+++ install_new_tutorials.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
-# This file will be updated when new tutorials are available for download!
-
 # first, a fix for the update_tutorials script found in the ARDroneUbuntu VM
-if [ -f "~/bin/update_tutorials" ];
+
+roscd ardrone_tutorials_getting_started
+
+if [ -f "/home/ardrone/bin/update_tutorials" ];
 then
- echo "Detected ARDroneUbuntu" 
- rm ~/bin/update_tutorials
- cp ARDroneUbuntu_Updates/update_tutorials ~/bin/update_tutorials
- chmod +x ~/bin/update_tutorials
+ echo "Detected ARDroneUbuntu, upgrading update_tutorials script" 
+ rm /home/ardrone/bin/update_tutorials
+ cp ARDroneUbuntu_Updates/update_tutorials /home/ardrone/bin/update_tutorials
+ chmod +x /home/ardrone/bin/update_tutorials
+fi
+
+if [ -f "/home/ardrone/bin/ardrone_keyboard_control" ];
+then
+ echo "Detected ARDroneUbuntu, upgrading ardrone_keyboard_control script" 
+ rm /home/ardrone/bin/ardrone_keyboard_control
+ cp ARDroneUbuntu_Updates/ardrone_keyboard_control /home/ardrone/bin/ardrone_keyboard_control
+ chmod +x /home/ardrone/bin/ardrone_keyboard_control
 fi
 
 roscd
 
-if [ -d "ardrone_tutorials_joystick_control" ];
+# remove the old tutorial structure
+
+if [ -d "ardrone_tutorials_getting_started" ];
 then
- cd ardrone_tutorials_joystick_control
- git pull
- roscd
-else
- git clone https://github.com/mikehamer/ardrone_tutorials_joystick_control.git
+ rm -rf ardrone_tutorials_getting_started
 fi
+
+# install the updated tutorial structure
+
+git clone https://github.com/mikehamer/ardrone_tutorials.git
